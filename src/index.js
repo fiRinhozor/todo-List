109a7db845b2719243833bb539d2b7f710bb052c
BULY <bulatgilfanov0@gmail.com>
2020-02-29 23:42:29 -0800

    Input validation added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
-import { Header } from "./app/header";
 import "../src/style.css";
 import "./styles/scss/main.scss";
 import "bootstrap";
-let header = new Header();
-let firstHeading = header.getFirstHeading();
-console.log(firstHeading);
+const itemInput = document.getElementById("list-input");
+const addButton = document.getElementById("add");
+const itemList = document.querySelector(".item-list");
+const enterValue = document.querySelector(".enter");
+let todoList = [];
+
+addButton.addEventListener("click", function () {
+    var inputSorted = itemInput.value;
+    var newElement = document.createElement("div");
+    newElement.innerHTML = inputSorted;
+
+    if (inputSorted == "") {
+        enterValue.innerHTML = "Please Enter Some Value";
+        enterValue.classList.add("showItem", "alert-danger");
+    } else if (inputSorted != "") {
+        enterValue.innerHTML = "";
+        enterValue.classList.remove("showItem", "alert-danger");
+    } else {
+        itemInput.value = "";
+    }
+    itemInput.value = "";
+
+    itemList.appendChild(newElement);
+
+
+});
