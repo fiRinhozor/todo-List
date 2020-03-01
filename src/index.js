40f22a849097c72a16bb661a154cdfa41c84e933
BULY <bulatgilfanov0@gmail.com>
2020-03-01 01:23:35 -0800

    delete button added

--- src/index.js
+++ src/index.js
@@ -5,25 +5,36 @@ const itemInput = document.getElementById("list-input");
 const addButton = document.getElementById("add");
 const itemList = document.querySelector(".item-list");
 const enterValue = document.querySelector(".enter");
+const clearButton = document.getElementById("clear-items");
 let todoList = [];
 
 addButton.addEventListener("click", function () {
-    var inputSorted = itemInput.value;
-    var newElement = document.createElement("div");
-    newElement.innerHTML = inputSorted;
+  var inputSorted = itemInput.value;
+  var newElement = document.createElement("div");
+  newElement.innerHTML = inputSorted;
 
-    if (inputSorted == "") {
-        enterValue.innerHTML = "Please Enter Some Value";
-        enterValue.classList.add("showItem", "alert-danger");
-    } else if (inputSorted != "") {
-        enterValue.innerHTML = "";
-        enterValue.classList.remove("showItem", "alert-danger");
-    } else {
-        itemInput.value = "";
-    }
+  //delete icon
+  const deleteBtn = document.createElement("button");
+  deleteBtn.className = "glyphicon glyphicon-remove";
+  deleteBtn.style.cssFloat = "right";
+
+  newElement.appendChild(deleteBtn);
+
+  if (inputSorted == "" || inputSorted == null) {
+    enterValue.innerHTML = "Please Enter Some Value";
+    enterValue.classList.add("showItem", "alert-danger");
+  } else if (inputSorted != "") {
+    enterValue.innerHTML = "";
+    enterValue.classList.remove("showItem", "alert-danger");
+  } else {
     itemInput.value = "";
+  }
+  itemInput.value = "";
 
-    itemList.appendChild(newElement);
 
+  itemList.appendChild(newElement);
+});
 
+clearButton.addEventListener("click", function () {
+  itemList.innerHTML = "";
 });
